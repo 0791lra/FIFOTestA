7b98f76f49ec78f9365fb23d7cac96fe46eb8a8d
Andrew Roberts <andrew@laurencefield.com>
2018-10-09 09:42:31 +0100

    Updated pop methods to match spec

--- Src/fifo.hpp
+++ Src/fifo.hpp
@@ -5,56 +5,67 @@
 #include <thread>
 #include <utility>
 
+/** \class Fifo
+ *  \brief Thread-safe FIFO queue
+ *
+ *  Wrapper for std::queue that provides thread-safety and limits the threads
+ *  capacity.
+ */
 template<class T> class Fifo
 {
 public:
-	Fifo(unsigned int capacity)
-		: qCapacity(capacity)
-	{
-	};
+    /// Create a Fifo with specified capacity
+    Fifo(unsigned int capacity)
+        : qCapacity(capacity)
+    {
+    };
 
-	bool push(const T& i)
-	{
-		std::lock_guard<std::mutex> pushLock(queueMutex);
-		if(fifoQ.size() >= qCapacity) {
-			return false;
-		}
-		fifoQ.push(i);
-		return true;
-	};
+    /// Push an item onto the stack unless it is full.
+    bool push(const T& i)
+    {
+        std::lock_guard<std::mutex> pushLock(queueMutex);
+        if(fifoQ.size() >= qCapacity) {
+            return false;
+        }
+        fifoQ.push(i);
+        return true;
+    };
 
-	T pop()
-	{
-		std::unique_lock<std::mutex> lock(queueMutex, std::try_to_lock);
-		while(!lock.owns_lock()){
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
-			lock.try_lock();
-		}
-		T retVal = fifoQ.front();
-		fifoQ.pop();
-		return retVal;
-	};
+    /// Pop an item off the stack, waiting for one to be available
+    T pop()
+    {
+        std::unique_lock<std::mutex> lock(queueMutex, std::adopt_lock);
+        while(fifoQ.size() == 0) {
+            lock.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            lock.lock();
+        }
+        T retVal = fifoQ.front();
+        fifoQ.pop();
+        return retVal;
+    };
 
-	std::pair<bool, T> pop_try()
-	{
-		std::pair<bool, T> retVal;
-		std::unique_lock<std::mutex> lock(queueMutex, std::try_to_lock);
-		if(!lock.owns_lock()){
-			retVal.first = false;
-			return retVal;
-		}
-		retVal.first = true;
-		retVal.second = fifoQ.front();
-		fifoQ.pop();
-		return retVal;
-	};
+    /// Pop an item off the stack, returning if none is available
+    std::pair<bool, T> pop_try()
+    {
+        std::pair<bool, T> retVal;
+        std::unique_lock<std::mutex> lock(queueMutex, std::adopt_lock);
+        if(fifoQ.size() > 0) {
+            retVal.first = true;
+            retVal.second = fifoQ.front();
+            fifoQ.pop();
+        }
+        else {
+            retVal.first = false;
+        }
+        return retVal;
+    };
 
 private:
-	std::queue<T> fifoQ;
-	unsigned int qCapacity;
+    std::queue<T> fifoQ; /*<! encapsulated queue */
+    unsigned int qCapacity; /*<! maximum number of items that can be queued */
 
-	// Mutex to protect the queue
-	std::mutex queueMutex;
+    std::mutex queueMutex; /*<! Mutex to protect the queue */
 };
 
 #endif
--- Src/fifoTests.cpp
+++ Src/fifoTests.cpp
@@ -5,57 +5,60 @@
 #include "arbitraryStruct.hpp"
 
 TEST_CASE("Fifo int", "Basic queue behaviour using ints") {
-	// zero capacity
-	Fifo<int>* f = new Fifo<int>(0);
-	REQUIRE(f != nullptr);
-	REQUIRE(!f->push(0));
+    // zero capacity
+    Fifo<int>* f = new Fifo<int>(0);
+    REQUIRE(f != nullptr);
+    REQUIRE(!f->push(0));
 
-	// capacity 1
-	Fifo<int>* f2 = new Fifo<int>(1);
-	REQUIRE(f2 != nullptr);
-	REQUIRE(f2->push(0));
-	REQUIRE(!f2->push(1));
+    // capacity 1
+    Fifo<int>* f2 = new Fifo<int>(1);
+    REQUIRE(f2 != nullptr);
+    REQUIRE(f2->push(0));
+    REQUIRE(!f2->push(1));
 
-	// capacity 2
-	Fifo<int>* f3 = new Fifo<int>(2);
-	REQUIRE(f3 != nullptr);
-	REQUIRE(f3->push(0));
-	REQUIRE(f3->push(1));
-	REQUIRE(!f3->push(2));	// Nope, full
-	REQUIRE(f3->pop() == 0);
-	REQUIRE(f3->push(2));	// Now there's space
-	REQUIRE(f3->pop() == 1);
-	REQUIRE(f3->pop() == 2);
+    // capacity 2
+    Fifo<int>* f3 = new Fifo<int>(2);
+    REQUIRE(f3 != nullptr);
+    REQUIRE(f3->push(0));
+    REQUIRE(f3->push(1));
+    REQUIRE(!f3->push(2));    // Nope, full
+    REQUIRE(f3->pop() == 0);
+    REQUIRE(f3->push(2));    // Now there's space
+    REQUIRE(f3->pop() == 1);
+    REQUIRE(f3->pop() == 2);
 }
 
 TEST_CASE("Fifo struct", "Basic queue behaviour using an arbitrary struct") {
-	// capacity 2
-	Fifo<arbitrary>* f3 = new Fifo<arbitrary>(2);
-	REQUIRE(f3 != nullptr);
-	REQUIRE(f3->push(0));
-	REQUIRE(f3->push(1));
-	REQUIRE(!f3->push(2));	// Nope, full
-	REQUIRE(f3->pop().anInt == 0);
-	REQUIRE(f3->push(2));	// Now there's space
-	REQUIRE(f3->pop().anInt == 1);
-	REQUIRE(f3->pop().anInt == 2);
+    // capacity 2
+    Fifo<arbitrary>* f3 = new Fifo<arbitrary>(2);
+    REQUIRE(f3 != nullptr);
+    REQUIRE(f3->push(0));
+    REQUIRE(f3->push(1));
+    REQUIRE(!f3->push(2));    // Nope, full
+    REQUIRE(f3->pop().anInt == 0);
+    REQUIRE(f3->push(2));    // Now there's space
+    REQUIRE(f3->pop().anInt == 1);
+    REQUIRE(f3->pop().anInt == 2);
 }
 
 TEST_CASE("Fifo pop_try", "Calling pop_try instead of pop") {
-	// capacity 2
-	Fifo<int>* f3 = new Fifo<int>(2);
-	REQUIRE(f3 != nullptr);
-	REQUIRE(f3->push(0));
-	REQUIRE(f3->push(1));
-	REQUIRE(!f3->push(2));	// Nope, full
-	std::pair<bool, arbitrary> returned = f3->pop_try();
-	REQUIRE(returned.first);
-	REQUIRE(returned.second.anInt == 0);
-	REQUIRE(f3->push(2));	// Now there's space
-	returned = f3->pop_try();
-	REQUIRE(returned.first);
-	REQUIRE(returned.second.anInt == 1);
-	returned = f3->pop_try();
-	REQUIRE(returned.first);
-	REQUIRE(returned.second.anInt == 2);
+    // capacity 2
+    Fifo<int>* f3 = new Fifo<int>(2);
+    REQUIRE(f3 != nullptr);
+    REQUIRE(f3->push(0));
+    REQUIRE(f3->push(1));
+    REQUIRE(!f3->push(2));    // Nope, full
+    std::pair<bool, arbitrary> returned = f3->pop_try();
+    REQUIRE(returned.first);
+    REQUIRE(returned.second.anInt == 0);
+    REQUIRE(f3->push(2));    // Now there's space
+    returned = f3->pop_try();
+    REQUIRE(returned.first);
+    REQUIRE(returned.second.anInt == 1);
+    returned = f3->pop_try();
+    REQUIRE(returned.first);
+    REQUIRE(returned.second.anInt == 2);
+    // Queue is empty now
+    returned = f3->pop_try();
+    REQUIRE(!returned.first);
 }
